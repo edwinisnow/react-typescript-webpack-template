be5303d8f38584b4f1dd333dd91c63551112dd3e
edwinisnow <edwin04@gmail.com>
2023-05-31 07:40:14 +0100

    icons, svg loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import "./styles.css";
+import IMAGE from "./react.png";
+import logo from "./logo.svg";
 
 export default function App() {
-  return <h1>React Typescript Webpack Starter Template</h1>;
+  return (
+    <>
+      <h1>React Typescript Webpack Starter Template</h1>;
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <img src={logo} alt="React Logo" width="300" height="200" />
+    </>
+  );
 }
