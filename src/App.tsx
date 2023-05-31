76e41f449d39f5f3c43de0429a9ff85cc5154022
edwinisnow <edwin04@gmail.com>
2023-05-31 08:02:33 +0100

    webpack common configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./styles.css";
 import IMAGE from "./react.png";
 import logo from "./logo.svg";
-
+// To run the build, goto build folder and run npx serve
 export default function App() {
   return (
     <>
-      <h1>React Typescript Webpack Starter Template</h1>;
+      <h1>React Typescript Webpack Starter Template {process.env.name}</h1>;
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={logo} alt="React Logo" width="300" height="200" />
     </>
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,10 @@
+const webpack = require("webpack");
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.name": JSON.stringify("react-dev"),
+    }),
+  ],
+};
