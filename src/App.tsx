fdfc4d352f9b23eab018af1c43b9fdeb723e7513
edwinisnow <edwin04@gmail.com>
2023-06-01 14:27:04 +0100

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import IMAGE from './react.png'
 import logo from './logo.svg'
 // To run the build, goto build folder and run npx serve
 export default function App() {
+  const name = 'Edwin'
   return (
     <>
-      <h1>React Typescript Webpack Starter Template {process.env.name}</h1>;
+      <h1>
+        {name} React Typescript Webpack Starter Template {process.env.name}
+      </h1>
+      ;
       <img src={IMAGE} alt="React Logo" width="300" height="200" />
       <img src={logo} alt="React Logo" width="300" height="200" />
     </>
