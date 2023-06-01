28229b1138325d8c30fd56a3a6fddb901c9468e9
edwinisnow <edwin04@gmail.com>
2023-06-01 09:14:14 +0100

    fixed minor code error

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('root'))
