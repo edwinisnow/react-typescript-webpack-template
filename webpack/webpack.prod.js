30a8c84143e436c2de47342879552d13e91096b8
edwinisnow <edwin04@gmail.com>
2023-06-01 14:58:37 +0100

    bundling

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,10 +1,14 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.name": JSON.stringify("react-prod"),
+      'process.env.name': JSON.stringify('react-prod'),
     }),
+    new BundleAnalyzerPlugin(),
   ],
-};
+}
